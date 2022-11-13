d0c71c4c31cfa04e875e3ffed52770858e2f82bd
Mamadou Mountagha BAH <mmountagha@cae.com>
2022-11-12 23:56:53 -0500

    on lexer...

---
+++ lexer.cpp
@@ -0,0 +1,34 @@
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+//===--------------------------------------------------------------------------===//
+// lexer
+//===--------------------------------------------------------------------------===//
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one 
+// of these for known things.
+
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;   // Filled in if tok_number
+
+// gettok - Return the next token from standard input.
+
+
